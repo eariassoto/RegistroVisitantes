7cb22f77556181eece98411af509561e98d55c70
Jeffry Venegas <jeffvene@gmail.com>
2016-04-12 12:58:18 -0600

    Reservas y visitantes

--- RegistroVisitantes/src/RegistroVisitantes/Controllers/Registro.cs
+++ RegistroVisitantes/src/RegistroVisitantes/Controllers/Registro.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,20 @@ namespace RegistroVisitantes.Controllers
     {
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: /Registro/Reservas
+        [HttpGet]
+        public IActionResult Reservas()
+        {
+            return View();
+        }
+
+        // GET: /Registro/Visitantes
+        [HttpGet]
+        public IActionResult Visitantes()
         {
             return View();
         }
